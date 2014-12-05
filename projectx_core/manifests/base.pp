19ee9cf3281c7c439893b425a1b96b61760e8310
Domenico Colandrea <dom@cainkade.com>
2014-12-05 14:19:59 -0500

    minor updates to nginx.conf, package versions, and vagrantfile

--- projectx_core/manifests/base.pp
+++ projectx_core/manifests/base.pp
@@ -3,23 +3,36 @@ $node_version = "v0.10.33"
 
 file { '/etc/motd':
 	content => "
-      .-----.
-    .' -   - '.        Project X
-   /  .-. .-.  \\      - Version: 0.0.1
-   |  | | | |  |
-    \\ \\o/ \\o/ /     - OS:      Official Ubuntu Server 64-bit 14.04
-   _/    ^    \\_      - Node:    ${node_version}
-  | \\  '---'  / |     - IP:      192.168.33.10
-  / /`--. .--`\\ \\    - Code:    ~/code/projectx
- / /'---` `---'\\ \\   - Author:  Domenico Colandrea
- '.__.       .__.'
-     `|     |`
-      |     \\
-      \\      '--.
-       '.        `\\
-         `'---.   |
-            ,__) /
-             `..'
+  Project X
+  - Version: 0.0.1
+  - OS:      Official Ubuntu Server 64-bit 14.04
+  - Node:    ${node_version}
+  - IP:      192.168.33.10
+  - Code:    ~/code/projectx
+  - Author:  Domenico Colandrea
+                                                          jjjj                                                  tttt
+                                                         j::::j                                              ttt:::t
+                                                          jjjj                                               t:::::t
+                                                                                                             t:::::t
+ppppp   ppppppppp   rrrrr   rrrrrrrrr      ooooooooooo  jjjjjjj    eeeeeeeeeeee        ccccccccccccccccttttttt:::::ttttttt                     xxxxxxx      xxxxxxx
+p::::ppp:::::::::p  r::::rrr:::::::::r   oo:::::::::::ooj:::::j  ee::::::::::::ee    cc:::::::::::::::ct:::::::::::::::::t                      x:::::x    x:::::x
+p:::::::::::::::::p r:::::::::::::::::r o:::::::::::::::oj::::j e::::::eeeee:::::ee c:::::::::::::::::ct:::::::::::::::::t                       x:::::x  x:::::x
+pp::::::ppppp::::::prr::::::rrrrr::::::ro:::::ooooo:::::oj::::je::::::e     e:::::ec:::::::cccccc:::::ctttttt:::::::tttttt     ---------------    x:::::xx:::::x
+ p:::::p     p:::::p r:::::r     r:::::ro::::o     o::::oj::::je:::::::eeeee::::::ec::::::c     ccccccc      t:::::t           -:::::::::::::-     x::::::::::x
+ p:::::p     p:::::p r:::::r     rrrrrrro::::o     o::::oj::::je:::::::::::::::::e c:::::c                   t:::::t           ---------------      x::::::::x
+ p:::::p     p:::::p r:::::r            o::::o     o::::oj::::je::::::eeeeeeeeeee  c:::::c                   t:::::t                                x::::::::x
+ p:::::p    p::::::p r:::::r            o::::o     o::::oj::::je:::::::e           c::::::c     ccccccc      t:::::t    tttttt                     x::::::::::x
+ p:::::ppppp:::::::p r:::::r            o:::::ooooo:::::oj::::je::::::::e          c:::::::cccccc:::::c      t::::::tttt:::::t                    x:::::xx:::::x
+ p::::::::::::::::p  r:::::r            o:::::::::::::::oj::::j e::::::::eeeeeeee   c:::::::::::::::::c      tt::::::::::::::t                   x:::::x  x:::::x
+ p::::::::::::::pp   r:::::r             oo:::::::::::oo j::::j  ee:::::::::::::e    cc:::::::::::::::c        tt:::::::::::tt                  x:::::x    x:::::x
+ p::::::pppppppp     rrrrrrr               ooooooooooo   j::::j    eeeeeeeeeeeeee      cccccccccccccccc          ttttttttttt                   xxxxxxx      xxxxxxx
+ p:::::p                                                 j::::j
+ p:::::p                                       jjjj      j::::j
+p:::::::p                                     j::::jj   j:::::j
+p:::::::p                                     j::::::jjj::::::j
+p:::::::p                                      jj::::::::::::j
+ppppppppp                                        jjj::::::jjj
+                                                    jjjjjj
 \n"
 }
 
--- projectx_core/modules/projectx/manifests/casperjs.pp
+++ projectx_core/modules/projectx/manifests/casperjs.pp
@@ -3,20 +3,20 @@
 class casperjs {
 
     file { "phantomjs-src":
-      path => "/home/vagrant/software/phantomjs-1.9.7-linux-x86_64.tar.bz2",
-      source => "puppet:///modules/projectx/software/phantomjs-1.9.7-linux-x86_64.tar.bz2",
+      path => "/home/vagrant/software/phantomjs-1.9.8-linux-x86_64.tar.bz2",
+      source => "puppet:///modules/projectx/software/phantomjs-1.9.8-linux-x86_64.tar.bz2",
     }
 
     exec { "extract-phantomjs":
         cwd => "/home/vagrant/software",
-        command => "/bin/tar xvjf phantomjs-1.9.7-linux-x86_64.tar.bz2",
-        creates => "/home/vagrant/software/phantomjs-1.9.7-linux-x86_64",
+        command => "/bin/tar xvjf phantomjs-1.9.8-linux-x86_64.tar.bz2",
+        creates => "/home/vagrant/software/phantomjs-1.9.8-linux-x86_64",
         require => [File["phantomjs-src"]]
     }
 
     exec { "link-phantomjs":
-        cwd => "/home/vagrant/software/phantomjs-1.9.7-linux-x86_64",
-        command => "/bin/ln -sf /home/vagrant/software/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs",
+        cwd => "/home/vagrant/software/phantomjs-1.9.8-linux-x86_64",
+        command => "/bin/ln -sf /home/vagrant/software/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs",
         creates => "/usr/local/bin/phantomjs",
         require => [Exec["extract-phantomjs"]]
     }
