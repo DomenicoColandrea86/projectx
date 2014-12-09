ddf5f31f4cbd16f95913202f367ee9121900047e
Domenico Colandrea <dom@cainkade.com>
2014-12-09 12:07:04 -0500

    remove comments

--- projectx/config/env/all.js
+++ projectx/config/env/all.js
@@ -8,24 +8,13 @@ module.exports = {
 	},
 	port: process.env.PORT || 3000,
 	templateEngine: 'swig',
-	// The secret should be set to a non-guessable string that
-	// is used to compute a session hash
 	sessionSecret: 'DOMS_SUPER_SESSION_SECRET',
-	// The name of the MongoDB collection to store sessions in
-	sessionCollection: 'sessions',
 	// The session cookie settings
 	sessionCookie: { 
 		path: '/',
 		httpOnly: true,
-		// If secure is set to true then it will cause the cookie to be set
-		// only when SSL-enabled (HTTPS) is used.
 		secure: true,
-		// Only set the maxAge to null if the cookie shouldn't be expired
-		// at all. The cookie will expunge when the browser is closed.
 		maxAge: null,
-		// To set the cookie in a specific domain uncomment the following 
-		// setting:
-		// domain: 'yourdomain.com'
 	},
 	// The session cookie name
 	sessionName: 'connect.sid',
